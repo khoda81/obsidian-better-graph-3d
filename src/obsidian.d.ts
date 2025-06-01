ee1687f0e72d77f574f2e3aa91e35ac57e45c5a2
Mahdi Khodabandeh <42507906+khoda81@users.noreply.github.com>
2025-06-01 16:03:42 +0330

    Integrated Obsidian

--- src/obsidian.d.ts
+++
@@ -1,4 +0,0 @@
-// filepath: c:\Users\20mah\Projects\better-3d-graph\src\obsidian.d.ts
-declare module 'obsidian' {
-    export * from '@types/obsidian'; // Use the existing type definitions if available
-}
